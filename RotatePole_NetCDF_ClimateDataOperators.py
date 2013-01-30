0772f8c639bcea23684c233030e38e1428969bec
Michael Lindgren <lindgren.mike@gmail.com>
2013-01-30 11:24:40 -0900

    some changes to get the thing on github in the way that I want.

--- RotatePole_NetCDF_ClimateDataOperators.py
+++ RotatePole_NetCDF_ClimateDataOperators.py
@@ -1,10 +1,10 @@
 import os, sys, re, glob
 
-files = glob.glob("/workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/r1i1p1/*.nc")
+files = glob.glob("/workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/r1i1p1/*sic_*.nc")
 
 for nc in files:
     print "working on: "+nc
-    os.system("cdo remapbil,t42grid "+nc+" "+os.path.join("/workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/t42/",os.path.basename(nc)[:-3]+"_t42grid.nc"))
+    os.system("cdo remapbil,t85grid "+nc+" "+os.path.join("/workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/t85/",os.path.basename(nc)[:-3]+"_t85grid.nc"))
 
 # this is the command to do this in cdo
 # cdo remapbil,t42grid /workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/r1i1p1/sic_OImon_GFDL-CM3_historical_r1i1p1_186001-186412.nc /workspace/Shared/Michael/CMIP5/data/OImon/mon/historical/r1i1p1/sic_OImon_GFDL-CM3_historical_r1i1p1_186001-186412_CHANGED.nc
--- Tracy_SeaIce_NSIDC_rasterize_t42grid.r
+++ Tracy_SeaIce_NSIDC_rasterize_t42grid.r
@@ -84,6 +84,41 @@ for(f in files){
 	writeRaster(rr, filename=paste(out_path,extension(basename(f),".tif"),sep=""), options="COMPRESS=LZW")
 }
 
+
+###############  TRY # 2
+
+library(raster)
+library(maptools)
+library(rgdal)
+library(sp)
+
+# we need a template t42grid for the rasterizing process
+# t42.pcll <- raster("/workspace/Shared/NSIDC_Monthly/SeaIceIndex/t42_template/global_t42grid_template.tif")
+# t42.greenwich <- rotate(raster("/workspace/Shared/NSIDC_Monthly/SeaIceIndex/t42_template/global_t42grid_template.tif"))
+out_path = "/workspace/Shared/NSIDC_Monthly/gsfc_output/converted_GTIFF/"
+files = list.files("/workspace/Shared/NSIDC_Monthly/gsfc_data", pattern=".bin$", full.names=T)
+
+for(f in files){
+
+	print(paste("working on: ",basename(f),sep=""))
+	pixel <- 25000 #pixel dimension in meters for both x and y 
+	xMin <- -3850000 #From NSIDC: ulxmap -3837500 
+	xMax <- xMin + (pixel*304) 
+	yMax <- 5850000 #From NSIDC: ulymap 5837500 
+	yMin <- yMax - (pixel*448) 
+	rr <- raster(nrow=448, ncol=304, xmn=xMin, xmx=xMax, ymn=yMin, ymx=yMax) 
+	# rr <- raster(matrix(bin,nrow=448,ncol=304,byrow=TRUE),xmn=-3850,xmx=3750,ymn=-5350,ymx=5850)
+	projection(rr) <- "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0" #'+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs' 
+
+	bin <- file(f,"rb")
+	read.bin <- readBin(bin, what="integer", n=136192, size=2, signed=FALSE, endian="little")
+	rr <- setValues(rr, (read.bin/10))
+
+	writeRaster(rr, filename=paste(out_path,extension(basename(f),".tif"),sep=""), options="COMPRESS=LZW")
+}
+
+
+
 # old shit below
 
 ##################################
